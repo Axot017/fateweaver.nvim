b9be1c81aeaad34e4bf6802cfa33a82af1572ef9
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-05-11 23:29:23 +0200

    feat: Track changes after insert

--- lua/fateweaver/changes.lua
+++ lua/fateweaver/changes.lua
@@ -8,64 +8,13 @@ local function slice_table(table, start, stop)
   return sliced
 end
 
+
 local M = {}
 
 local changes_history = {}
 local buffer_cache = {}
 
-function M.setup()
-  vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
-    pattern = "*",
-    callback = function(args)
-      M.cache_buffer(args.buf)
-    end
-  })
-
-  vim.api.nvim_create_autocmd("InsertEnter", {
-    pattern = "*",
-    callback = function(args)
-      M.cache_buffer(args.buf)
-    end
-  })
-
-  vim.api.nvim_create_autocmd("TextChanged", {
-    pattern = "*",
-    callback = function(args)
-      M.record_change_instant(args.buf)
-    end
-  })
-
-  vim.api.nvim_create_autocmd("TextChangedI", {
-    pattern = "*",
-    callback = function(args)
-      -- M.record_change(args.buf)
-    end
-  })
-
-  logger.debug("Change tracking autocommands created")
-end
-
--- Cache the current state of a buffer
-function M.cache_buffer(bufnr)
-  bufnr = bufnr or vim.api.nvim_get_current_buf()
-  local filename = vim.api.nvim_buf_get_name(bufnr)
-
-  if filename == "" then
-    return
-  end
-
-  local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
-
-  buffer_cache[bufnr] = {
-    filename = filename,
-    lines = lines,
-    timestamp = os.time()
-  }
-
-  logger.debug("Cached buffer " .. bufnr .. " (" .. filename .. ")" .. " with " .. #lines .. " lines")
-end
-
-function M.record_change_instant(bufnr)
+local function calculate_change(bufnr)
   bufnr = bufnr or vim.api.nvim_get_current_buf()
   local filename = vim.api.nvim_buf_get_name(bufnr)
 
@@ -101,7 +50,7 @@ function M.record_change_instant(bufnr)
 
   for j = change_begin, current_length do
     if previous_lines[change_begin] == current_lines[j] then
-      change_end = j
+      change_end = j - 1
       changed_from = slice_table(previous_lines, change_begin, change_begin)
       changed_to = slice_table(current_lines, change_begin, j)
       break
@@ -110,7 +59,7 @@ function M.record_change_instant(bufnr)
 
   for j = change_begin, previous_length do
     if current_lines[change_begin] == previous_lines[j] then
-      change_end = j
+      change_end = j - 1
       changed_from = slice_table(previous_lines, change_begin, j)
       changed_to = slice_table(current_lines, change_begin, change_begin)
       break
@@ -119,22 +68,80 @@ function M.record_change_instant(bufnr)
 
   for j = change_begin, max_length do
     if previous_lines[j] == current_lines[j] then
-      change_end = j
-      changed_from = slice_table(previous_lines, change_begin, j - 1)
-      changed_to = slice_table(current_lines, change_begin, j - 1)
+      change_end = j - 1
+      changed_from = slice_table(previous_lines, change_begin, j)
+      changed_to = slice_table(current_lines, change_begin, j)
       break
     end
   end
 
-
-  changes_history[#changes_history + 1] = {
+  return {
     filename = filename,
     change_begin = change_begin,
     change_end = change_end,
     changed_from = changed_from,
     changed_to = changed_to,
+    timestamp = os.time(),
+    bufnr = bufnr
+  }
+end
+
+function M.setup()
+  vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+    pattern = "*",
+    callback = function(args)
+      M.cache_buffer(args.buf)
+    end
+  })
+
+  vim.api.nvim_create_autocmd("InsertLeave", {
+    pattern = "*",
+    callback = function(args)
+      M.record_change(args.buf)
+    end
+  })
+
+  vim.api.nvim_create_autocmd("TextChanged", {
+    pattern = "*",
+    callback = function(args)
+      M.record_change(args.buf)
+    end
+  })
+
+  logger.debug("Change tracking autocommands created")
+end
+
+-- Cache the current state of a buffer
+function M.cache_buffer(bufnr)
+  bufnr = bufnr or vim.api.nvim_get_current_buf()
+  local filename = vim.api.nvim_buf_get_name(bufnr)
+
+  if filename == "" then
+    return
+  end
+
+  local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+
+  buffer_cache[bufnr] = {
+    filename = filename,
+    lines = lines,
     timestamp = os.time()
   }
+
+  logger.debug("Cached buffer " .. bufnr .. " (" .. filename .. ")" .. " with " .. #lines .. " lines")
+end
+
+function M.record_change(bufnr)
+  bufnr = bufnr or vim.api.nvim_get_current_buf()
+  local new_change = calculate_change(bufnr)
+
+  table.insert(changes_history, new_change)
+
+  logger.debug("Recorded change: " .. vim.inspect(new_change))
+
+  local filename = vim.api.nvim_buf_get_name(bufnr)
+
+  local current_lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
 
   buffer_cache[bufnr] = {
     filename = filename,
