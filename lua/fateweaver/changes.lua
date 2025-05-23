a9b5bb7933f8d02c681dd6f9695250e008449a7a
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-05-23 08:16:39 +0200

    feat: Ignore non file buffers

--- lua/fateweaver/changes.lua
+++ lua/fateweaver/changes.lua
@@ -40,25 +40,59 @@ local function calculate_change(bufnr)
   }
 end
 
+local function is_real_file(bufnr)
+  bufnr = bufnr or vim.api.nvim_get_current_buf()
+
+  local filename = vim.api.nvim_buf_get_name(bufnr)
+  if filename == "" then
+    return false
+  end
+
+  local buf_type = vim.bo[bufnr].buftype
+  if buf_type ~= "" then
+    return false
+  end
+
+  local filetype = vim.bo[bufnr].filetype
+  local skip_filetypes = {
+    "qf", "help", "terminal", "oil", "fugitive", "NvimTree",
+    "TelescopePrompt", "dirvish", "netrw"
+  }
+
+  for _, ft in ipairs(skip_filetypes) do
+    if filetype == ft then
+      return false
+    end
+  end
+
+  return true
+end
+
 function M.setup()
   vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
     pattern = "*",
     callback = function(args)
-      M.cache_buffer(args.buf)
+      if is_real_file(args.buf) then
+        M.cache_buffer(args.buf)
+      end
     end
   })
 
   vim.api.nvim_create_autocmd("InsertLeave", {
     pattern = "*",
     callback = function(args)
-      M.save_change(args.buf)
+      if is_real_file(args.buf) then
+        M.save_change(args.buf)
+      end
     end
   })
 
   vim.api.nvim_create_autocmd("TextChanged", {
     pattern = "*",
     callback = function(args)
-      M.save_change(args.buf)
+      if is_real_file(args.buf) then
+        M.save_change(args.buf)
+      end
     end
   })
 
