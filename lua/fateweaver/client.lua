47b27262d160f75e501f215a10ff955409c13553
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-05-25 21:30:09 +0200

    refactor: Move zeta specific concepts to client.lua

--- lua/fateweaver/client.lua
+++ lua/fateweaver/client.lua
@@ -7,14 +7,113 @@ if not curl_ok then
   return
 end
 
+local function get_completion_lines(completion_str)
+  local start_marker = "<|editable_region_start|>"
+  local end_marker = "<|editable_region_end|>"
+
+  local start_pos = string.find(completion_str, start_marker, 1, true)
+  local end_pos = string.find(completion_str, end_marker, 1, true)
+
+  if not start_pos or not end_pos then
+    return {}
+  end
+
+  local content_start = start_pos + string.len(start_marker)
+
+  completion_str = string.sub(completion_str, content_start, end_pos - 1)
+
+  if completion_str:sub(1, 1) == "\n" then
+    completion_str = completion_str:sub(2)
+  end
+
+  if completion_str:sub(-1) == "\n" then
+    completion_str = completion_str:sub(1, -2)
+  end
+
+  local completions = vim.split(completion_str, "\n")
+
+  return completions
+end
+
+
+local prompt_template =
+"### Instruction:\nYou are a code completion assistant and your task is to analyze user edits and then rewrite an excerpt that the user provides, suggesting the appropriate edits within the excerpt, taking into account the cursor location.\n\n### User Edits:\n\n%s### User Excerpt:\n\n```%s\n%s\n```\n\n### Response:\n\n"
+
+local edit_template = "User edited \"%s\":\n```diff\n%s\n```"
+
+local function get_buffer_with_tokens(bufnr, editable_region, cursor_pos)
+  local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+
+  local cursor_line = cursor_pos[1]
+  local cursor_col = cursor_pos[2]
+
+  local cursor_line_text = lines[cursor_line]
+  if cursor_line_text then
+    lines[cursor_line] = string.sub(cursor_line_text, 1, cursor_col) ..
+        "<|user_cursor_is_here|>" ..
+        string.sub(cursor_line_text, cursor_col + 1)
+  end
+
+  local context_offet = config.get().context_offset
+
+  local start_line = math.max(1, cursor_line - context_offet)
+  local end_line = math.min(#lines, cursor_line + context_offet)
+  local included_lines = {}
+
+  for i = start_line, end_line do
+    table.insert(included_lines, lines[i])
+  end
+
+  local result = {}
+
+  if start_line == 1 then
+    table.insert(result, "<|start_of_file|>")
+  end
+
+  for i, line in ipairs(included_lines) do
+    local actual_line_num = start_line + i - 1
+
+    if actual_line_num == editable_region.start_line then
+      table.insert(result, "<|editable_region_start|>")
+    end
+
+    table.insert(result, line)
+
+    if actual_line_num == editable_region.end_line then
+      table.insert(result, "<|editable_region_end|>")
+    end
+  end
+
+  if end_line == #lines then
+    table.insert(result, "<|end_of_file|>")
+  end
+
+  return table.concat(result, "\n")
+end
+
+local function get_prompt(bufnr, editable_region, cursor_pos, changes)
+  local formatted_changes = ""
+  for _, change in pairs(changes) do
+    formatted_changes = formatted_changes .. string.format(edit_template, change.filename, change.diff) .. "\n\n"
+  end
+
+  local buffer_text = get_buffer_with_tokens(bufnr, editable_region, cursor_pos)
+
+  local prompt = string.format(prompt_template, formatted_changes, vim.api.nvim_buf_get_name(bufnr), buffer_text)
+
+  logger.debug("Prompt:\n\n" .. prompt)
+
+  return prompt
+end
+
 local M = {}
 
-function M.request_completion(prompt, callback)
+function M.request_completion(bufnr, editable_region, cursor_pos, changes, callback)
   local url = config.get().endpoint
   local model = config.get().model
   local body = {
     model = model,
-    prompt = prompt,
+    prompt = get_prompt(bufnr, editable_region, cursor_pos, changes),
     stream = false,
   }
 
@@ -31,7 +130,10 @@ function M.request_completion(prompt, callback)
       end
 
       local reponse_body = vim.json.decode(res.body)
-      callback(reponse_body["response"])
+      local response = reponse_body["response"]
+      local proposed_completions = get_completion_lines(response)
+
+      callback(proposed_completions)
     end
   })
 end
--- lua/fateweaver/completer.lua
+++ lua/fateweaver/completer.lua
@@ -1,7 +1,6 @@
 local changes = require("fateweaver.changes")
 local client = require("fateweaver.client")
 local logger = require("fateweaver.logger")
-local prompt_manager = require("fateweaver.prompt_manager")
 
 local function get_editable_region()
   local win_top = vim.fn.line('w0')
@@ -16,34 +15,6 @@ local function get_editable_region_lines(bufnr, editable_region)
   return lines
 end
 
-local function get_completion_lines(completion_str)
-  local start_marker = "<|editable_region_start|>"
-  local end_marker = "<|editable_region_end|>"
-
-  local start_pos = string.find(completion_str, start_marker, 1, true)
-  local end_pos = string.find(completion_str, end_marker, 1, true)
-
-  if not start_pos or not end_pos then
-    return {}
-  end
-
-  local content_start = start_pos + string.len(start_marker)
-
-  completion_str = string.sub(completion_str, content_start, end_pos - 1)
-
-  if completion_str:sub(1, 1) == "\n" then
-    completion_str = completion_str:sub(2)
-  end
-
-  if completion_str:sub(-1) == "\n" then
-    completion_str = completion_str:sub(1, -2)
-  end
-
-  local completions = vim.split(completion_str, "\n")
-
-  return completions
-end
-
 local request_bufnr = -1
 
 local M = {}
@@ -54,26 +25,19 @@ function M.propose_completions(bufnr)
   local cursor_pos = vim.api.nvim_win_get_cursor(0)
   local editable_region = get_editable_region()
 
-  local prompt = prompt_manager.get_prompt(bufnr, editable_region, cursor_pos, diffs)
-
   local editable_region_lines = get_editable_region_lines(bufnr, editable_region)
   local editable_region_lines_str = table.concat(editable_region_lines, "\n")
   logger.debug("Editable region lines:\n\n" .. editable_region_lines_str)
 
-  client.request_completion(prompt, function(completions)
+  client.request_completion(bufnr, editable_region, cursor_pos, diffs, function(completions)
     if request_bufnr ~= bufnr then
       return
     end
 
-    local proposed_completions = get_completion_lines(completions)
-    if #proposed_completions == 0 then
-      return
-    end
+    local completions_str = table.concat(completions, "\n")
+    logger.debug("Proposed completions:\n\n" .. completions_str)
 
-    local proposed_completions_str = table.concat(proposed_completions, "\n")
-    logger.debug("Proposed completions:\n\n" .. proposed_completions_str)
-
-    local diff = vim.diff(editable_region_lines_str, proposed_completions_str, {
+    local diff = vim.diff(editable_region_lines_str, completions_str, {
       result_type = "indices"
     })
 
