e9a2dc7be40ba8f7038deedeb49249d49a0a24ba
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-05-28 08:05:38 +0200

    refactor: listeners

--- lua/fateweaver/listeners.lua
+++ lua/fateweaver/listeners.lua
@@ -4,7 +4,7 @@ local changes = require("fateweaver.changes")
 local debouncer = require("fateweaver.debouncer")
 local completer = require("fateweaver.completer")
 
-local function is_real_file(bufnr)
+local function is_buffer_supported(bufnr)
   bufnr = bufnr or vim.api.nvim_get_current_buf()
 
   local filename = vim.api.nvim_buf_get_name(bufnr)
@@ -32,62 +32,61 @@ local function is_real_file(bufnr)
   return true
 end
 
+local function checked_callback(callback)
+  return function(args)
+    if is_buffer_supported(args.buf) then
+      callback(args)
+    end
+  end
+end
+
 local M = {}
 
 function M.setup()
   vim.api.nvim_create_autocmd({ "BufEnter" }, {
     pattern = "*",
-    callback = function(args)
-      if is_real_file(args.buf) then
-        changes.track_buffer(args.buf)
-      end
-    end
+    callback = checked_callback(function(args)
+      changes.track_buffer(args.buf)
+    end)
   })
 
   vim.api.nvim_create_autocmd({ "BufLeave", "InsertLeave" }, {
     pattern = "*",
-    callback = function(args)
-      if is_real_file(args.buf) then
-        debouncer.cancel(args.buf)
-        completer.clear()
-      end
-    end
+    callback = checked_callback(function(args)
+      debouncer.cancel(args.buf)
+      completer.clear()
+    end)
   })
 
   vim.api.nvim_create_autocmd("InsertLeave", {
     pattern = "*",
-    callback = function(args)
-      if is_real_file(args.buf) then
-        changes.save_change(args.buf)
-      end
-    end
+    callback = checked_callback(function(args)
+      changes.save_change(args.buf)
+    end)
   })
 
   vim.api.nvim_create_autocmd("TextChanged", {
     pattern = "*",
-    callback = function(args)
-      if is_real_file(args.buf) then
-        changes.save_change(args.buf)
-      end
-    end
+    callback = checked_callback(function(args)
+      changes.save_change(args.buf)
+    end)
   })
 
   vim.api.nvim_create_autocmd({ "TextChangedP", "TextChangedI" }, {
     pattern = "*",
-    callback = function(args)
+    callback = checked_callback(function(args)
       if vim.g.fateweaver_pause_completion then
         vim.g.fateweaver_pause_completion = false
         return
       end
-      if is_real_file(args.buf) then
-        local debounce_time = config.get().debounce_ms
-        local bufnr = args.buf
-        debouncer.debounce(debounce_time, args.buf, function()
-          local additional_change = changes.calculate_change(args.buf)
-          completer.propose_completions(bufnr, additional_change)
-        end)
-      end
-    end
+
+      local debounce_time = config.get().debounce_ms
+      local bufnr = args.buf
+      debouncer.debounce(debounce_time, args.buf, function()
+        local additional_change = changes.calculate_change(args.buf)
+        completer.propose_completions(bufnr, additional_change)
+      end)
+    end)
   })
 
   logger.debug("Change tracking autocommands created")
