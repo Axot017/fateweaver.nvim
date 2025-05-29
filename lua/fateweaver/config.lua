4af9f3a0002d606d07f9e26c127bccfaa68f1926
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-05-29 09:48:53 +0200

    docs: add type comments to config

--- lua/fateweaver/config.lua
+++ lua/fateweaver/config.lua
@@ -1,5 +1,16 @@
+---@class Config
+---@field log_level string Level of logging ("ERROR", "WARN", "INFO", "DEBUG")
+---@field logger_fn fun(msg: string): nil Function used for logging
+---@field max_changes_in_context integer Maximum number of changes to keep in context
+---@field max_tracked_buffers integer Maximum number of buffers to track
+---@field endpoint string API endpoint URL for generation
+---@field model string AI model identifier to use
+---@field context_offset integer Number of lines to include before/after for context
+---@field debounce_ms integer Debounce time in milliseconds
+
 local M = {}
 
+---@type Config
 local _default_config = {
   log_level = "ERROR",
   logger_fn = vim.notify,
@@ -11,15 +22,18 @@ local _default_config = {
   debounce_ms = 1000
 }
 
+---@type Config
 local _config = _default_config
 
-
+---@param new_config? Config
+---@return nil
 M.setup = function(new_config)
   local c = new_config or {}
 
   _config = vim.tbl_extend("force", _default_config, c)
 end
 
+---@return Config
 M.get = function()
   return _config
 end
